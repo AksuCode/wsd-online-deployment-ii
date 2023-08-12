3afe5e4f968f422c4ec5a50cc2b875e28889cba6
AksuCode <parsaviherio@gmail.com>
2023-08-12 15:30:18 +0300

    Completely different ii -> iii

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import { serve } from "./deps.js";
-import { configure, renderFile } from "./deps.js";
+import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+import * as messageService from "./services/messageService.js";
 
 configure({
   views: `${Deno.cwd()}/views/`,
@@ -9,23 +10,48 @@ const responseDetails = {
   headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
-const data = {
-  count: 0,
-  msg: "Visits: ",
+const redirectTo = (path) => {
+  return new Response(`-`, {
+    status: 303,
+    headers: {
+      "Location": path,
+    },
+  });
 };
 
+let id = 0;
+
+const addMessage = async (request) => {
+  const formDat = await request.formData();
+  await messageService.create(id, formDat.get("sender"), formDat.get("message"));
+  id++;
+}
+
 const handleRequest = async (request) => {
+  
+  const method = request.method;
   const url = new URL(request.url);
-  if (url.pathname === "/visits") {
-    data.count++;
-    return new Response(await renderFile("count.eta", data), responseDetails);
+  const path = url.pathname;
+
+  if (method === "GET" && path === "/") {
+
+    const data = {
+      messages: await messageService.lastFive(),
+    }
+
+    return new Response(await renderFile("index.eta", data), responseDetails);
   }
 
-  else if (url.pathname === "/meaning") {
-    return new Response("Seeking truths beyond meaning of life, you will find 43.");
+  else if (method === "POST" && path === "/") {
+    await addMessage(request);
+
+    return redirectTo("/");
   }
 
-  return new Response("Nothing here yet.");
+  else {
+    return redirectTo("/");
+  }
+
 };
 
 serve(handleRequest, { port: 7777 });
---
+++ services/messageService.js
@@ -0,0 +1,11 @@
+import { sql } from "./database/database.js";
+
+const lastFive = async () => {
+    return await sql`SELECT * FROM messages ORDER BY id DESC LIMIT 5`;
+}
+
+const create = async (id, sender, message) => {
+    await sql`INSERT INTO messages (id, sender, message) VALUES (${id}, ${sender}, ${message})`;
+}
+
+export { lastFive, create }
